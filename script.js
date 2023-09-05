62630d3173444dd89e74c61ab6ac8476f2bb3d61
MyPc <brianw119@outlook.com>
2023-09-05 22:26:02 +0100

    async function added to detect user's location

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
 
+
+
 const weatherMap = {
 
     baseURL            :"http://api.weatherapi.com/v1",
     apiKey             :"83c85d12493343dfa1d173629232808",
+
+    baseURL2           :"https://api.geoapify.com/v1",
+    apiKey2            :"3168d1c4bdac4b6c9f105582e696da33",
+
+
+
 
     location           :"cheshunt",
     coordinates        :[],
@@ -13,21 +21,25 @@ const weatherMap = {
     currentCloudCover  :[],
     currentAtmPressure :[],
     tempFeelsLike      :[],
+    userLocationCurrent:"",
+    locationCoordinates:[],
 
 
 
     detectLocalCondition(){
-       
-        return `${this.baseURL}/current.json?key=${this.apiKey}&q=${this.location}&qi=no`;
-
+       return `${this.baseURL}/current.json?key=${this.apiKey}&q=${this.location}&qi=no`;
     },
 
 
     weatherForecast(){
-
-        console.log(`${this.baseURL}/forecast.json?key=${this.apiKey}&q=${this.location}&days=1&aqi=no&alerts=no`);
-
+      return `${this.baseURL}/forecast.json?key=${this.apiKey}&q=${this.location}&days=1&aqi=no&alerts=no`;
     },
+
+    detectCurrentLocation(){
+        return `${this.baseURL2}/ipinfo?&apiKey=${this.apiKey2}`;
+    },
+
+
 
     //stores coordinates of location in array "coordinates"
     async getLocation(){
@@ -39,15 +51,26 @@ const weatherMap = {
         return this.coordinates;
     },
 
+    //pushes user's current location as lat. and lon. to array
+    async getUserLocation(){
+        
+        locationCoordinates = [];
+        const response = await fetch(this.detectCurrentLocation());
+        const result = await response.json();
+        this.locationCoordinates.push(result.location.latitude);
+        this.locationCoordinates.push(result.location.longitude);
+        console.log(this.locationCoordinates);
+    },
+
     //stores current atmospheric conditions in arrays
     async currentAtmospheric(){
 
-        
         this.temperatureNow = [];
         this.currentAtmospheric = [];
         this.currentCloudCover = [];
         this.humidityNow = [];
         this.tempFeelsLike = [];
+        this.currentCondition = [];
 
         const response = await fetch(this.detectLocalCondition());
         const result = await response.json();
@@ -57,7 +80,7 @@ const weatherMap = {
         this.currentCloudCover.push(result.current.cloud);
         this.currentAtmPressure.push(result.current.pressure_mb);
         this.tempFeelsLike.push(result.current.feelslike_c);
-        console.log(this.tempFeelsLike);
+        console.log(this.temperatureNow);
     },
 
 
@@ -85,18 +108,7 @@ const weatherMap = {
 
             chart.draw(data, options);
         };
-        this.currentAtmospheric();
+        this.getUserLocation();
     },
 };
 weatherMap.init();
-
-
-
-
-
-
-
-
-
-
-
